cd8d6219ceeb367edec98755c9a383ca7f1453bf
Vitalii Rybachuk <vitalii.rybachuk@infopulse.com>
2020-10-13 23:27:31 +0300

    fix package.json and imports

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 //utils
-const {getPort} = require('./utils/getport');
+const getPort = require('./utils/getport');
 
 //routes
 const {userRouter} = require('./routes')
